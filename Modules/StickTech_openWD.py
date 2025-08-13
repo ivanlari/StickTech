f8cda4615aeb018516d27ad87c25f292b08d68d1
Ivan Lari <121730929+ivanlari@users.noreply.github.com>
2025-08-13 17:35:05 +0200

    Delete Modules directory

--- Modules/StickTech_openWD.py
+++
@@ -1,7 +0,0 @@
-def openWD(savepath):
-    import os
-    import logging
-    from datetime import datetime
-    print("\n" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "    Working directory is opened.")
-    logging.info(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "    Working directory is opened.")
-    os.startfile(savepath)
